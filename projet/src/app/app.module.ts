f6f8869da4a8a2c06b95d8712e3671e56b4e0318
SchamaK <schamakouassi198@gmail.com>
2022-05-12 08:54:21 +0000

    nav en material

--- projet/src/app/app.module.ts
+++ projet/src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Acceuil/nav/nav.component';
+import { MainComponent } from './Acceuil/main/main.component';
+import { FooterComponent } from './Acceuil/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// material
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // material
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
